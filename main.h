df587dabc2f4ee0eaca88ebc5daa1b961453d957
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-10 23:17:48 -0500

    adding a header file and a firt test of shell

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct get
+{
+    char *shape;
+    int (*f)(va_list);
+} get_fun;
+
+#endif
