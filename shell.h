6a7deb1a46ad1e9749d163c114f2add6d3bb468e
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-04-18 07:28:16 -0500

    Header file

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+char *findpath(char *comando);
+char *str_concat(char *str1, char *str2);
+int str_lenght(char *string);
+char str_compare(char *str1, char *str2, int n);
+char *_getenv(const char *name);
+void print_env(void);
+#endif
